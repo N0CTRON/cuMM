0318ac8d70e0c7a4a9ebb8553e1853492ffdf911
XeTute <154016609+N0CTRON@users.noreply.github.com>
2024-05-05 14:32:31 +0200

    Added resize

--- cuMM.cu
+++ cuMM.cu
@@ -7,9 +7,15 @@ template <typename devType>
 class cuMM
 {
 public:
-	devType* data;
+	devType* data = nullptr;
+	size_t currentSizeBytes = NULL;
 
-	void malloc(size_t elems) { cudaMalloc(&data, sizeof(devType) * elems); }
+	void malloc(size_t elems)
+	{
+		currentSizeBytes = sizeof(devType) * elems;
+		cudaMalloc(&data, currentSizeBytes);
+	}
+
 	void free()
 	{
 		if (data != nullptr) //<-- Branching neccessary :(
@@ -21,10 +27,23 @@ public:
 
 	void resize(size_t newSize)
 	{
-		return; //Next commit
+		devType* newPtr;
+		cudaMalloc(&newPtr, newSize);
+		if (newPtr != nullptr)
+		{
+			cudaMemcpy(newPtr, data, currentSizeBytes < newSize ? currentSizeBytes : newSize, cudaMemcpyHostToHost);
+			cudaFree(data);
+		}
+		data = newPtr;
 	};
 
 	cuMM() {};
-	cuMM(size_t elems) { cudaMalloc(&data, sizeof(devType) * elems); }; //same as cuMalloc cuz overhead 'n stuff
+
+	cuMM(size_t elems) //same as cuMalloc cuz overhead 'n stuff
+	{
+		currentSizeBytes = sizeof(devType) * elems;
+		cudaMalloc(&data, currentSizeBytes);
+	};
+
 	~cuMM() { std::thread(free); }
 };
